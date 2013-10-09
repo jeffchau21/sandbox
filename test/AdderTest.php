77ec8d28c50109a89977286a6f7585f9ba149467
Jeff Chau <jeff.chau@wellintech.com.hk>
2013-10-09 16:03:44 +0800

    should be ok

--- test/AdderTest.php
+++ test/AdderTest.php
@@ -1,12 +1,13 @@
 <?php
 
-require_once '../Adder.php';
+require_once 'Adder.php';
 
 class AdderTest extends PHPUnit_Framework_TestCase
 {
     public function testAdd()
     {
-	$c = $this->add(3,4);
+	$obj = new Adder;
+	$c = $obj->add(3,4);
 	$this->assertEquals(6,$c);
     }
  
